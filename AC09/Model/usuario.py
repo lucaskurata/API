b9accbf3c4cb582000030d21cebb1221309f17d5
Israel Souto dos Santos <1800866@SALAS.AULAS>
2019-05-09 20:59:45 -0300

    Comiits

---
+++ AC09/Model/usuario.py
@@ -0,0 +1,8 @@
+class Usuario():
+    def __init__(self, idUsuario, nome, segredo):
+        self.idUsuario = idUsuario
+        self.nome = nome
+        self.segredo = segredo
+
+    def criar(nome):
+        
