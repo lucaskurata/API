9018e0f4ceda357b7486fb18e912c5e9684b2070
Israel Souto dos Santos <1800866@SALAS.AULAS>
2019-05-09 21:41:18 -0300

    OOK

--- AC09/usuario_api.py
+++ AC09/usuario_api.py
@@ -3,3 +3,7 @@ from flask import Blueprint, jsonify, request
 
 usuarios_app = Blueprint('usuarios_app',__name__,template_folder='templates')
 
+
+@usuarios_app.route('/usr', methods=['POST'])
+def cadastrar():
+    
